711406ed46344d6dfc6848c798c6aae3f9589567
Carlos Benetti <benetticarlos1@gmail.com>
2022-01-26 18:31:52 -0300

    navbar

--- paw-house/src/App.js
+++ paw-house/src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world desde react!
-        </p>
-      </header>
-    </div>
+<NavBar />
   );
 }
 
---
+++ paw-house/src/components/NavBar.js
@@ -0,0 +1,55 @@
+import { AppBar, Button, makeStyles, Toolbar } from '@material-ui/core';
+import logo from '../logo-paw.svg'
+import React from 'react';
+
+const useStyles = makeStyles({
+  bar: {
+    display: 'flex',
+    width: '100%',
+  },
+  toolbar: {
+    display: 'flex',
+    width: '90%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  logo: {
+    display: 'flex',
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  img: {
+    display: 'flex',
+    width: '10vh'
+  },
+  content: {
+    width: '80%',
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+});
+
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar className={classes.bar}>
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.logo}>
+            <img className={classes.img} src={logo} />
+          </div>
+          <div className="content">
+            <Button>informacion</Button>
+            <Button>tienda</Button>
+            <Button>adopción</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default NavBar;
