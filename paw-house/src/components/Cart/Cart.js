4b975c67f992a73a4d6f8d6fd773c2473f0eca7d
Carlos Benetti <benetticarlos1@gmail.com>
2022-02-23 21:58:20 -0300

    cart work

--- paw-house/src/components/Cart/Cart.js
+++ paw-house/src/components/Cart/Cart.js
@@ -1,10 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { CartContext } from '../../context/CartContext';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  img: {
+    width: '200px',
+    height: '200px',
+  },
+});
 
 export const Cart = () => {
-  const { cart, vaciarCarrito } = useContext(CartContext);
+    const classes = useStyles();
+  const { cart, vaciarCarrito, totalPrice, eliminarItem } =
+    useContext(CartContext);
+  console.log(cart)
 
-  console.log(cart);
 
   return (
     <div>
@@ -12,11 +22,25 @@ export const Cart = () => {
         <h2>tu carrito está vacio</h2>
       ) : (
         <>
-          {cart.map((producto) => (
-            <div key={producto.id}>
-              <h3>{producto.name}</h3>
-            </div>
-          ))}
+          {
+          cart.map((producto) => {
+            console.log(producto);
+            return (
+              <div key={producto.id}>
+                <button onClick={() => eliminarItem(producto.id, producto.cantidad, producto.price)}>X</button>
+
+                <img
+                  src={producto.img}
+                  alt="imagen del producto"
+                  className={classes.img}
+                />
+                <h2>{producto.name}</h2>
+                <h3>cantidad: {producto.cantidad}</h3>
+                <h3>${producto.price}</h3>
+              </div>
+            );
+          })}
+          <h2>Precio total: ${totalPrice}</h2>
           <button onClick={vaciarCarrito}>vaciar carrito</button>
         </>
       )}
--- paw-house/src/context/CartContext.js
+++ paw-house/src/context/CartContext.js
@@ -1,12 +1,11 @@
-import {createContext, useContext, useState } from 'react'
-import {ItemListContainer} from '../components/ItemListContainer/ItemListContainer'
-import { products } from '../api/api'
+import {createContext, useState } from 'react'
 
 export const CartContext = createContext([]);
 
 const CartContextProvider = ({children}) => {
 
   const [cart, setCart] = useState([])
+  const [totalPrice, setTotalPrice] = useState(0)
 
   const isOnCart = (id) => {
     const res = cart.some((prod) => prod.id === id)
@@ -19,18 +18,36 @@ const CartContextProvider = ({children}) => {
       alert('Este producto ya está en el carrito!')
     } else {
       setCart([...cart, { ...item, cantidad: counter }]);
+      setTotalPrice(totalPrice + (counter * item.price))
     }
   }
 
   const vaciarCarrito = () => {
+    setTotalPrice(0)
     setCart([])
   }
 
+  const eliminarItem = (id, cantidad, precio) => {
+
+      const newCart = cart.filter((item) => item.id !== id);
+      setCart(newCart);
+    console.log('cantidad :>> ', cantidad);
+    console.log('precio :>> ', precio);
+
+
+ newCart.length !== 0
+   ? setTotalPrice(totalPrice - cantidad * precio)
+   : setTotalPrice(0);
+
+
+  }
 
 
   
   return (
-    <CartContext.Provider value={{ addToCart, cart, vaciarCarrito }}>
+    <CartContext.Provider
+      value={{ addToCart, cart, vaciarCarrito, totalPrice, eliminarItem }}
+    >
       {children}
     </CartContext.Provider>
   );
