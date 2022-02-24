923513ee2ce7200085dc936c0466ba2c9e09041a
Carlos Benetti <benetticarlos1@gmail.com>
2022-02-23 22:52:06 -0300

    cart widget count

--- paw-house/src/context/CartContext.js
+++ paw-house/src/context/CartContext.js
@@ -1,56 +1,58 @@
-import {createContext, useState } from 'react'
+import { createContext, useState } from 'react';
 
 export const CartContext = createContext([]);
 
-const CartContextProvider = ({children}) => {
-
-  const [cart, setCart] = useState([])
-  const [totalPrice, setTotalPrice] = useState(0)
+const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
 
   const isOnCart = (id) => {
-    const res = cart.some((prod) => prod.id === id)
-    return res
-  }
-
+    const res = cart.some((prod) => prod.id === id);
+    return res;
+  };
 
   const addToCart = (counter, item) => {
     if (isOnCart(item.id)) {
-      alert('Este producto ya está en el carrito!')
+      alert('Este producto ya está en el carrito!');
     } else {
       setCart([...cart, { ...item, cantidad: counter }]);
-      setTotalPrice(totalPrice + (counter * item.price))
+      setTotalPrice(totalPrice + counter * item.price);
+      setTotalItems(totalItems + counter);
     }
-  }
+  };
 
   const vaciarCarrito = () => {
-    setTotalPrice(0)
-    setCart([])
-  }
+    setTotalPrice(0);
+    setCart([]);
+    setTotalItems(0);
+  };
 
   const eliminarItem = (id, cantidad, precio) => {
+    const newCart = cart.filter((item) => item.id !== id);
+    setCart(newCart);
 
-      const newCart = cart.filter((item) => item.id !== id);
-      setCart(newCart);
-    console.log('cantidad :>> ', cantidad);
-    console.log('precio :>> ', precio);
+    newCart.length !== 0
+      ? setTotalPrice(totalPrice - cantidad * precio)
+      : setTotalPrice(0);
 
+    setTotalItems(totalItems - cantidad);
+  };
 
- newCart.length !== 0
-   ? setTotalPrice(totalPrice - cantidad * precio)
-   : setTotalPrice(0);
-
-
-  }
-
-
-  
   return (
     <CartContext.Provider
-      value={{ addToCart, cart, vaciarCarrito, totalPrice, eliminarItem }}
+      value={{
+        addToCart,
+        cart,
+        vaciarCarrito,
+        totalPrice,
+        eliminarItem,
+        totalItems,
+      }}
     >
       {children}
     </CartContext.Provider>
   );
-}
+};
 
-export default  CartContextProvider
+export default CartContextProvider;
